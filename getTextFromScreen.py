f50d75d3b96f402516081f3a30ead4b3e189f1ed
tburton12 <p.wrzesien@o2.pl>
2019-02-12 22:32:28 +0100

    Tray icon, key listeners, settings object, generating settings file. Everything works

--- getTextFromScreen.py
+++ getTextFromScreen.py
@@ -1,15 +1,66 @@
-from pynput.keyboard import Listener
+from pynput.keyboard import Listener as KeyboardListener
+from pynput.mouse import Listener as MouseListener
+from pynput.mouse import Button as MouseButton
 from settings import program_settings
+# import pyscreenshot
 
 
-def on_press(key):
+def on_mouse_click(x, y, button, pressed):
+    global press_coordinates
+    global release_coordinates
+
+    # If button were pressed
+    if button == MouseButton.left:
+        if pressed:
+            print("Left button pressed")
+            press_coordinates = {'detected': True, 'button': button, 'event': 'pressed', 'x': x, 'y': y}
+        # If button were released
+        else:
+            print("Left button released")
+            release_coordinates = {'detected': True, 'button': button, 'event': 'released', 'x': x, 'y': y}
+            # Stop listening
+            return False
+    # Cancel snapping if mouse right button clicked
+    elif button == MouseButton.right:
+        print("Right button pressed")
+        press_coordinates = {'detected': False}
+        release_coordinates = {'detected': False}
+        # Stop listening
+        return False
+
+
+press_coordinates = {}
+release_coordinates = {}
+
+
+def take_screenshot():
+    # screenshot = pyscreenshot.grab()
+
+    print("Mouse listener attached")
+    with MouseListener(
+            on_click=on_mouse_click) as listener:
+        listener.join()
+
+        # if snapping has not been cancelled
+        if press_coordinates['detected'] and release_coordinates['detected']:
+            try:
+                print("Press: ", press_coordinates['x'], press_coordinates['y'])
+                print("Release: ", release_coordinates['x'], release_coordinates['y'])
+            except:
+                pass
+
+
+def on_keyboard_press(key):
+    print(str(key), " : ", program_settings.capture_action_key)
     if str(key) == program_settings.capture_action_key:
         print(key, " pressed")
+        take_screenshot()
 
 
 def attach_listener():
     # Detects key presses
-    with Listener(
-            on_press=on_press
+    print("Key listener attached")
+    with KeyboardListener(
+            on_press=on_keyboard_press
             ) as listener:
         listener.join()
--- programHandler.py
+++ programHandler.py
@@ -6,9 +6,9 @@ from threading import Thread
 def main():
 
     # Create program handler
-    get_text_from_screen_handler = Thread(target=attach_listener)
-    get_text_from_screen_handler.setDaemon(True)
-    get_text_from_screen_handler.start()
+    listener = Thread(target=attach_listener)
+    listener.setDaemon(True)
+    listener.start()
 
     # Create tray icon
     icon = trayIcon.TrayIcon(title='Text capture')
