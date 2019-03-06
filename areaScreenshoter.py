1691aeff9cd8cabd4bb6bec9d5474b5175cfd931
tburton12 <p.wrzesien@o2.pl>
2019-03-06 17:53:51 +0100

    Revert "Changed Tesseract path"
    This reverts commit d503db6c8357dc7e3ab2da15daf50f734d22cdfa.

--- areaScreenshoter.py
+++ areaScreenshoter.py
@@ -17,6 +17,7 @@ class AreaScreenshoter:
         """
         Create fullscreen window and print image given in param
         :param img: Image to be printed in window
+        :return:
         """
         # Create window root
         root = Tk()
